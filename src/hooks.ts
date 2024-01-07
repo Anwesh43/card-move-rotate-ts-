6c0205f65e6bd751383d125222147340a3cdd435
Anwesh43 <anweshthecool0@gmail.com>
2024-01-07 20:48:11 +0530

    adding useDimension hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -27,3 +27,26 @@ export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) : U
         }
     }
 }
+
+interface UseDimensionProp {
+    w : number, 
+    h : number 
+}
+export const useDimension = () : UseDimensionProp => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        const listener = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        window.addEventListener('resize', listener, false)
+        return () => {
+            window.removeEventListener('resize', listener)
+        }
+    }, [])
+    return {
+        w, 
+        h
+    }
+}
